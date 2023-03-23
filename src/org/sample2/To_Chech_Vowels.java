805205bf8cfeea4bd849a00424f17aa553c7f0bf
SathishChidambaram <sathishchidambaram77@gmail.com>
2023-03-24 00:39:29 +0530

    Uploading

---
+++ src/org/sample2/To_Chech_Vowels.java
@@ -0,0 +1,19 @@
+package org.sample2;
+
+public class To_Chech_Vowels {
+
+	public static void main(String[] args) {
+
+		String str = "SATHISH";
+		
+		String st = str.toLowerCase();
+
+		if (st.contains("a")|st.contains("e")|st.contains("i")|st.contains("o")|st.contains("u")) {
+			System.out.println("Contains Vowel");
+		}
+		else {
+			System.out.println("Not Contains Vowels");
+		}
+	}
+
+}
